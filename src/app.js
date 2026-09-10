519e05c20f5f833f5ca08a7734e43a0d8556f8b0
aradhana-prasad2244 <aradhanaprasad2210@gmail.com>
2026-09-10 11:26:44 +0530

    Install multer package

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const multer = require('multer');
 
 const app = express();
 
 app.use(express.json());
+const upload = multer({ storage: multer.memoryStorage() });
+
+app.post('/create-post', upload.single('image'), async(req,res)=>{
+    console.log(req.file);
+    console.log(req.body);
+})
 
 module.exports = app;
