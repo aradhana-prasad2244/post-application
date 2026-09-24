39241a33e5c887b28f8d4ed4a9d57958adcde2c7
aradhana-prasad2244 <aradhanaprasad2210@gmail.com>
2026-09-24 22:03:12 +0530

    Add GET API to fetch all posts

--- src/app.js
+++ src/app.js
@@ -25,27 +25,12 @@ app.post("/create-post", upload.single("image"), async (req, res) => {
   });
 });
 
-
-// app.post("/create-post", upload.single("image"), async (req, res) => {
-
-//     console.log("BODY:", req.body);
-//     console.log("FILE:", req.file ? "FILE RECEIVED" : "NO FILE");
-
-//     const result = await uploadFile(req.file.buffer);
-
-//     console.log("IMAGEKIT RESULT:", result.url);
-
-//     const post = await postModel.create({
-//         image: result.url,
-//         caption: req.body.caption
-//     });
-
-//     console.log("MONGODB POST:", post);
-
-//     return res.status(201).json({
-//         message: "Post created successfully",
-//         post
-//     });
-// });
+app.get("/posts", async (req, res) => {
+  const posts = await postModel.find();
+  return res.status(200).json({ 
+    message: "Posts fetched successfully",
+    posts 
+  });
+});
 
 module.exports = app;
