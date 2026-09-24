386547f19efdcb17b13fde3c27a4d3211eda611a
aradhana-prasad2244 <aradhanaprasad2210@gmail.com>
2026-09-24 21:23:11 +0530

    Implement image upload and create post API

--- src/app.js
+++ src/app.js
@@ -1,19 +1,51 @@
-const express = require('express');
-const multer = require('multer');
+const express = require("express");
+const multer = require("multer");
+const uploadFile = require("./services/storage.service");
+const postModel = require("./models/post.model");
 
 const app = express();
 app.use(express.json());
 
 const upload = multer({ storage: multer.memoryStorage() });
 
-app.post("/create-post", upload.single("image"), async(req,res)=>{
-    console.log(req.body)
-    console.log(req.file)
+app.post("/create-post", upload.single("image"), async (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
 
-    res.status(201).json({
-        success: true,
-        message: "Post created successfully"
-    })
-}) 
+  const result = await uploadFile(req.file.buffer);
+
+  const post = await postModel.create({
+    image: result.url,
+    caption: req.body.caption,
+  });
+
+  return res.status(201).json({
+    message: "Post created successfully",
+    post,
+  });
+});
+
+
+// app.post("/create-post", upload.single("image"), async (req, res) => {
+
+//     console.log("BODY:", req.body);
+//     console.log("FILE:", req.file ? "FILE RECEIVED" : "NO FILE");
+
+//     const result = await uploadFile(req.file.buffer);
+
+//     console.log("IMAGEKIT RESULT:", result.url);
+
+//     const post = await postModel.create({
+//         image: result.url,
+//         caption: req.body.caption
+//     });
+
+//     console.log("MONGODB POST:", post);
+
+//     return res.status(201).json({
+//         message: "Post created successfully",
+//         post
+//     });
+// });
 
 module.exports = app;
--- src/db/db.js
+++ src/db/db.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose');
 
 async function connectDB(){
-    await mongoose.connect('mongodb+srv://aradhanaprasad2210:qV02F1cGTxBqVrl7@notescluster.rlsbkju.mongodb.net/post-application');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to the DB');
 }
 
 module.exports = connectDB;
+
+// const mongoose = require('mongoose');
+
+// async function connectDB() {
+//     await mongoose.connect(process.env.MONGO_URI);
+
+//     console.log('Connected to the DB');
+//     console.log('Database:', mongoose.connection.name);
+// }
+
+// module.exports = connectDB;
